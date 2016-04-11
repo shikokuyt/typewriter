da37cbd44c4ff750fdcdb6500cad245c9b5f5927
maczyt <369280416@qq.com>
2016-04-12 07:39:41 +0800

    fix load bug

--- src/js/typewrite.js
+++ src/js/typewrite.js
@@ -20,7 +20,7 @@ var typewrite = (function (window, undefined) {
     }
     function init(selector, opts) {
       var ele = $(selector);
-      ele.style.cssText = "transition: all ease-in 1s .1s; box-shadow: 0 0 6px #f66; border-radius: 5px;padding: 20px;font-family: '微软雅黑' arial; color: #fff; text-shadow: 1px 1px #ccc; background-color: #c33;position: absolute;left: 50%;top: 50%;max-width: 450px;transform: translate(-50%, -50%);"
+      ele.style.cssText = "text-indent: 1em; transition: all ease-in 1s .1s; box-shadow: 0 0 6px #f66; border-radius: 5px;padding: 20px;font-family: '微软雅黑' arial; color: #fff; text-shadow: 1px 1px #ccc; background-color: #c33;position: absolute;left: 50%;top: 50%;max-width: 450px;transform: translate(-50%, -50%);"
       var str = ele.innerHTML || "You do not have to enter the value you want to set oh !!!";
       str =  str.replace('/<|>/g', '');
       opts = extend(opts, {
